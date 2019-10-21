34b43e8fd0869190f2922ec62fbf34b5ae8b38eb
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-10-21 10:59:20 +0200

    Añadido la linea enviada por el cliente

--- server.py
+++ server.py
@@ -16,14 +16,15 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
-        self.wfile.write(b"Hemos recibido tu peticion")
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
+        self.wfile.write(b"Hemos recibido tu peticion: " + bytes(line))
+
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
+    # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler) 
+    serv = socketserver.UDPServer(('', 6001), EchoHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
