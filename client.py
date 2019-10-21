ae7ef09fdb664911283a904f0d879ce6e48f4a56
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-10-21 10:58:57 +0200

    Añadido los argumentos por la shell

--- client.py
+++ client.py
@@ -2,19 +2,26 @@
 """
 Programa cliente UDP que abre un socket a un servidor
 """
-
+import sys
 import socket
 
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = 'localhost'
 PORT = 6001
-LINE = '¡Hola mundo!'
+
+
+#Leemos los argumentos desde la shell
+
+if len(sys.argv) < 4:
+    sys.exit("Usage: client.py ip puerto linea")
+
+Line = ' '.join(sys.argv[3:])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    print("Enviando:", Line)
+    my_socket.send(bytes(Line, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
