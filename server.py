628bdb7a8b30d9434d2533d814cfe1d2ac5b1ccf
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-10-23 20:19:44 +0200

    Ejercicio 6 hecho, devuelve ip y puerto del cliente, y pide el parametro puerto por la shell

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
 
 
 class EchoHandler(socketserver.DatagramRequestHandler):
@@ -16,15 +17,22 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         handle method of the server class
         (all requests will be handled by this method)
         """
+        IP_client, Puerto_client = self.client_address
         for line in self.rfile:
-            print("El cliente nos manda ", line.decode('utf-8'))
-
+            print('El cliente con IP ' + str(IP_client) + " y Puerto: " + str(Puerto_client) + ' nos manda: ', line.decode('utf-8'))
         self.wfile.write(b"Hemos recibido tu peticion: " + bytes(line))
+         # self.request is the TCP socket connected to the client
+
+
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 server.py Puerto")
+
+    Puerto = sys.argv[1]
+    serv = socketserver.UDPServer(('', int(Puerto)), EchoHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
