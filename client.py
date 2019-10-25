79ce9c71c1a5a83fdc37ab22cc7ad73c03bd82c5
anapgh <ap.garcia.2016@alumnos.urjc.es>
2019-10-25 14:52:56 +0200

    ejer 8

--- client.py
+++ client.py
@@ -10,20 +10,22 @@ try:
     SERVER = sys.argv[1]
     PORT = int(sys.argv[2])
     USER = sys.argv[4]
+    EXPIRES_VALUE = sys.argv[5]
 except IndexError or ValueError:
-    sys.exit('Usage: python3 client.py server port REGISTER user')
+    sys.exit('Usage: python3 client.py localhost port REGISTER sip_address expires_value ')
 
 
 if sys.argv[3] == 'REGISTER':
-    Line = ('REGISTER' + ' sip:' + USER + ' SIP/2.0\\r\\n\\r\\n')
+    Lines = ('REGISTER' + ' sip:' + USER + ' SIP/2.0\r\n' +
+            'Expires: ' + str(EXPIRES_VALUE))
 else :
-    Line = sys.argv[3]
+    Lines = sys.argv[3]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", Line)
-    my_socket.send(bytes(Line, 'utf-8') + b'\r\n')
+    print("Enviando:", Lines)
+    my_socket.send(bytes(Lines, 'utf-8') + b'\r\n\r\n')
     data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
